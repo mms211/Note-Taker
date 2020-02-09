fd13e189dfe4e5e1b0f08020c5a18ae92249430d
Morgan Sommer <morgana.sommer@gmail.com>
2020-02-09 15:24:14 -0500

    api path added to server.js

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-//require("./paths/apiRoutes")(app);
+require("../../paths/apiRoutes")(app);
 require("../../paths/htmlRoutes")(app);
 
 // =============================================================================
