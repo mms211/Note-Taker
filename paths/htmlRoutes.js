881c719a6196cfac7a58993b75e5dfbd8bc2aee0
Morgan Sommer <morgana.sommer@gmail.com>
2020-02-12 21:03:36 -0500

    small changes

--- paths/htmlRoutes.js
+++ paths/htmlRoutes.js
@@ -1,30 +1,12 @@
-// ===============================================================================
-// DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
-// ===============================================================================
 var path = require("path");
 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-
 module.exports = function(app) {
-  // HTML GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
-  // ---------------------------------------------------------------------------
-
-  // app.get("/", function(req, res) {
-  //   console.log("In / Route");
-  //   res.sendFile(path.join(__dirname, "../html/index.html"));
-  // });
 
   app.get("/notes", function(req, res) {
     console.log("In Notes Route");
     res.sendFile(path.join(__dirname, "../public/html/notes.html"));
   });
 
-  // If no matching route is found default to home
   app.get("*", function(req, res) {
     console.log("In * Route");
     res.sendFile(path.join(__dirname, "../public/html/index.html"));
--- server.js
+++ server.js
@@ -1,30 +1,14 @@
-// Dependencies
-// =============================================================
 var express = require("express");
 
-// Sets up the Express App
-// =============================================================
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// ================================================================================
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
-
 require("./paths/apiRoutes")(app);
 require("./paths/htmlRoutes")(app);
-
-// =============================================================================
-// LISTENER
-// The below code effectively "starts" our server
-// =============================================================================
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
